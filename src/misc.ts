dbefc35417fb4e81f28fe67e44e18e4f7ffa5348
Konstantin Alikhanov <atomAltera@gmail.com>
2018-11-25 13:49:58 +0400

    Avoiding to use .check method. All logic on .then

--- src/misc.ts
+++ src/misc.ts
@@ -1,6 +1,6 @@
 export const tuple = <T extends any[]>(...items: T) => items;
 
-export const optionalConverter = <A, B>(f: (a: A) => B) =>
+export const optional = <A, B>(f: (a: A) => B) =>
     (a: A | null | undefined) =>
         a === null ? null : a === undefined ? undefined : f(a);
 
--- src/treat-like.test.ts
+++ src/treat-like.test.ts
@@ -1,6 +1,8 @@
+import {concat} from "ramda";
 import {createChain} from "./constructors";
-import {optionalConverter, optionalValidator} from "./misc";
+import {optional} from "./misc";
 import {treatLike} from "./treat-like";
+import {Validator} from "./types";
 
 const tuple = <T extends any[]>(...args: T) => args;
 
@@ -13,40 +15,62 @@ interface Example {
     error: any;
 }
 
-// custom validators
-const onlyString = (value: any): string => {
-    if (typeof value === "string") {
-        return value;
-    } else {
-        throw new Error("Is not string");
-    }
-};
+// experiments
+const createValidator = <A>(f: Validator<A>) =>
+    (value: A) => {
+        if (f(value)) {
+            return value;
+        } else {
+            throw new Error("Validation failed");
+        }
+    };
 
-const onlyProvided = <T>(value: T | null | undefined): T => {
-    if (value !== undefined && value !== null) {
-        return value;
-    } else {
-        throw new Error("Is not string");
-    }
-};
+const createTypeValidator = <I, O>(f: (value: I | O) => value is O) =>
+    (value: any): O => {
+        if (f(value)) {
+            return value as O;
+        } else {
+            throw new Error("Valve failed");
+        }
+    };
+
+// type predicates
+const isDefined = <A>(value: unknown): value is {} => (value !== undefined && value !== null);
+const isString = (value: unknown): value is string => typeof value === "string";
+
+// value predicates
+
+const longer = <T extends string | any[]>(then: number) => (value: T) => value.length > then;
+const notEmpty = longer(0);
+
+// type validators
+const onlyProvided = createTypeValidator(isDefined);
+const onlyString = createTypeValidator(isString);
+
+// value validators
+const onlyNotEmpty = <T extends string | any[]>(v: T) => createValidator<T>(notEmpty)(v);
+const onlyLonger = (then: number) =>
+    <T extends string | any[]>(value: T) => createValidator<T>(longer(then))(value);
 
 // shortcuts
 const optionalStringField = createChain()
-    .then(optionalConverter(onlyString), "err_not_string")
+    .then(optional(onlyString), "err_not_string")
+    .then(optional(onlyLonger(1)), "err_too_short")
+    .then(optional((x) => x.toUpperCase()))
 ;
 
 const requiredStringField = createChain()
     .then(onlyProvided, "err_required")
     .then(onlyString, "err_not_string")
+    .then(onlyLonger(1), "err_too_short")
+    .then((x) => x.toUpperCase())
 ;
 
-const examples: Example[] = [
+const fieldExamples: Example[] = [
     {
         note: "ok -> optional field",
         ok: true,
-        schema: optionalStringField
-            .check(optionalValidator((x) => x.length > 1), "err_too_short")
-            .then(optionalConverter((x) => x.toUpperCase())),
+        schema: optionalStringField,
         input: "Hello",
         output: "HELLO",
         error: undefined,
@@ -55,9 +79,7 @@ const examples: Example[] = [
     {
         note: "invalid -> optional field",
         ok: false,
-        schema: optionalStringField
-            .check(optionalValidator((x) => x.length > 1), "err_too_short")
-            .then(optionalConverter((x) => x.toUpperCase())),
+        schema: optionalStringField,
         input: "1",
         output: undefined,
         error: "err_too_short",
@@ -66,9 +88,7 @@ const examples: Example[] = [
     {
         note: "wrong type -> optional field",
         ok: false,
-        schema: optionalStringField
-            .check(optionalValidator((x) => x.length > 1), "err_too_short")
-            .then(optionalConverter((x) => x.toUpperCase())),
+        schema: optionalStringField,
         input: 4,
         output: undefined,
         error: "err_not_string",
@@ -77,9 +97,7 @@ const examples: Example[] = [
     {
         note: "undefined -> optional field",
         ok: true,
-        schema: optionalStringField
-            .check(optionalValidator((x) => x.length > 1), "err_too_short")
-            .then(optionalConverter((x) => x.toUpperCase())),
+        schema: optionalStringField,
         input: undefined,
         output: undefined,
         error: undefined,
@@ -88,9 +106,7 @@ const examples: Example[] = [
     {
         note: "ok -> required field",
         ok: true,
-        schema: requiredStringField
-            .check((x) => x.length > 1, "err_too_short")
-            .then((x) => x.toUpperCase()),
+        schema: requiredStringField,
         input: "Hello",
         output: "HELLO",
         error: undefined,
@@ -99,9 +115,7 @@ const examples: Example[] = [
     {
         note: "invalid -> required field",
         ok: false,
-        schema: requiredStringField
-            .check((x) => x.length > 1, "err_too_short")
-            .then((x) => x.toUpperCase()),
+        schema: requiredStringField,
         input: "1",
         output: undefined,
         error: "err_too_short",
@@ -110,9 +124,7 @@ const examples: Example[] = [
     {
         note: "wrong type -> required field",
         ok: false,
-        schema: requiredStringField
-            .check((x) => x.length > 1, "err_too_short")
-            .then((x) => x.toUpperCase()),
+        schema: requiredStringField,
         input: 4,
         output: undefined,
         error: "err_not_string",
@@ -121,15 +133,38 @@ const examples: Example[] = [
     {
         note: "undefined -> required field",
         ok: false,
-        schema: requiredStringField
-            .check((x) => x.length > 1, "err_too_short")
-            .then((x) => x.toUpperCase()),
+        schema: requiredStringField,
         input: undefined,
         output: undefined,
         error: "err_required",
     },
 
 ];
+
+const listExamples: Example[] = [
+    // {
+    //     note: "ok -> required not empty list",
+    //     ok: true,
+    //     schema: createChain()
+    //         .then(onlyProvided, "err_required")
+    //         .then(onlyList, "err_not_list")
+    //         .then(onlyNotEmpty)
+    //         .then([
+    //             requiredStringField
+    //             .check(optionalValidator((x) => x.length > 1), "err_too_short")
+    //             .then(optionalConverter((x) => x.toUpperCase())),
+    //         ]),
+    //
+    //     input: ["Hello", "World"],
+    //     output: ["HELLO", "WORLD"],
+    //     error: undefined,
+    // },
+];
+
+const examples: Example[] = [
+    fieldExamples,
+    listExamples,
+].reduce(concat, []);
 
 describe("sets ok status as expected", () => {
     examples.map((e) => test(e.note, async () => {
