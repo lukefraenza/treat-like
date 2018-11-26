53747d6318f94bdaf33ef51596472d8e3a95b0af
Konstantin Alikhanov <atomAltera@gmail.com>
2018-11-26 12:04:55 +0400

    Some minor changes in tests

--- src/treat-like.test.ts
+++ src/treat-like.test.ts
@@ -2,7 +2,7 @@ import {concat} from "ramda";
 import {createChain} from "./constructors";
 import {optional} from "./misc";
 import {treatLike} from "./treat-like";
-import {Validator} from "./types";
+import {FullOutput, Input, Validator} from "./types";
 
 const tuple = <T extends any[]>(...args: T) => args;
 
@@ -15,7 +15,7 @@ interface Example {
     error: any;
 }
 
-// experiments
+// experiment sontructors
 const createValidator = <A>(f: Validator<A>) =>
     (value: A) => {
         if (f(value)) {
@@ -34,45 +34,63 @@ const createTypeValidator = <I, O>(f: (value: I | O) => value is O) =>
         }
     };
 
+const fromSchema = <S>(schema: S) =>
+    (value: Input<S>): Promise<FullOutput<S>> => {
+        return treatLike(schema, value)
+            .then((report) => {
+                if (report.ok) {
+                    return report.value;
+                } else {
+                    throw new Error("converting error");
+                }
+            });
+    };
+
 // type predicates
 const isDefined = <A>(value: unknown): value is {} => (value !== undefined && value !== null);
 const isString = (value: unknown): value is string => typeof value === "string";
+const isList = (value: unknown): value is Array<unknown> => Array.isArray(value);
+
 
 // value predicates
-
 const longer = <T extends string | any[]>(then: number) => (value: T) => value.length > then;
 const notEmpty = longer(0);
+
 
 // type validators
 const onlyProvided = createTypeValidator(isDefined);
 const onlyString = createTypeValidator(isString);
+const onlyList = createTypeValidator(isList);
+
 
 // value validators
 const onlyNotEmpty = <T extends string | any[]>(v: T) => createValidator<T>(notEmpty)(v);
-const onlyLonger = (then: number) =>
-    <T extends string | any[]>(value: T) => createValidator<T>(longer(then))(value);
+const onlyLonger = (then: number) => <T extends string | any[]>(value: T) => createValidator<T>(longer(then))(value);
+
 
 // shortcuts
 const optionalStringField = createChain()
     .then(optional(onlyString), "err_not_string")
     .then(optional(onlyLonger(1)), "err_too_short")
-    .then(optional((x) => x.toUpperCase()))
+    .then(optional((x) => x.trim()))
 ;
 
 const requiredStringField = createChain()
     .then(onlyProvided, "err_required")
     .then(onlyString, "err_not_string")
     .then(onlyLonger(1), "err_too_short")
-    .then((x) => x.toUpperCase())
+    .then((x) => x.trim())
 ;
 
+
+// examples
 const fieldExamples: Example[] = [
     {
         note: "ok -> optional field",
         ok: true,
         schema: optionalStringField,
-        input: "Hello",
-        output: "HELLO",
+        input: "  Hello   ",
+        output: "Hello",
         error: undefined,
     },
 
@@ -107,8 +125,8 @@ const fieldExamples: Example[] = [
         note: "ok -> required field",
         ok: true,
         schema: requiredStringField,
-        input: "Hello",
-        output: "HELLO",
+        input: "  Hello",
+        output: "Hello",
         error: undefined,
     },
 
@@ -145,18 +163,57 @@ const listExamples: Example[] = [
     // {
     //     note: "ok -> required not empty list",
     //     ok: true,
-    //     schema: createChain()
-    //         .then(onlyProvided, "err_required")
-    //         .then(onlyList, "err_not_list")
-    //         .then(onlyNotEmpty)
-    //         .then([
-    //             requiredStringField
-    //             .check(optionalValidator((x) => x.length > 1), "err_too_short")
-    //             .then(optionalConverter((x) => x.toUpperCase())),
-    //         ]),
+    //     schema: (
+    //         createChain()
+    //             .then(onlyProvided, "err_required")
+    //             .then(onlyList, "err_not_list")
+    //             .then(onlyNotEmpty)
+    //             .then(reverseEachElementInList_required)
+    //     ),
     //
     //     input: ["Hello", "World"],
-    //     output: ["HELLO", "WORLD"],
+    //     output: ["olleH", "dlroW"],
+    //     error: undefined,
+    // },
+
+    // {
+    //     note: "undefined -> optional not empty list",
+    //     ok: true,
+    //     schema: (
+    //         createChain()
+    //             .then(optional(onlyList), "err_not_list")
+    //             .then(optional(onlyNotEmpty))
+    //             .then(optional(reverseEachElementInList_optional))
+    //     ),
+    //
+    //     input: undefined,
+    //     output: undefined,
+    //     error: undefined,
+    // },
+    //
+    // {
+    //     note: "ok -> optional not empty list",
+    //     ok: true,
+    //     schema: (
+    //         createChain()
+    //             .then(optional(onlyList), "err_not_list")
+    //             .then(optional(onlyNotEmpty))
+    //             .then(reverseEachElementInList_optional)
+    //     ),
+    //
+    //     input: ["foo", "bar"],
+    //     output: ["oof", "rab"],
+    //     error: undefined,
+    // },
+];
+
+const otherExamples: Example[] = [
+    // {
+    //     note: "Embedding chain with ok data",
+    //     ok: true,
+    //     schema: requiredStringField.then(fromSchema(reversed_required), "aaa"),
+    //     input: "     hello ",
+    //     output: "olleh",
     //     error: undefined,
     // },
 ];
@@ -164,8 +221,11 @@ const listExamples: Example[] = [
 const examples: Example[] = [
     fieldExamples,
     listExamples,
+    otherExamples,
 ].reduce(concat, []);
 
+
+// tests
 describe("sets ok status as expected", () => {
     examples.map((e) => test(e.note, async () => {
         const report = await treatLike(e.schema, e.input);
